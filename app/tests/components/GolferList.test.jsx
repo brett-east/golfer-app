a3e068b1baf68f28495a698d724c25b1edb6b15c
Brett East <brett@sproutback.com>
2017-04-20 19:38:57 +1000

    Test GolferList is rendering GolferProfiles

--- app/tests/components/GolferList.test.jsx
+++ app/tests/components/GolferList.test.jsx
@@ -5,9 +5,38 @@ import TestUtils from 'react-addons-test-utils';
 
 
 import GolferList from 'GolferList';
+import GolferProfile from 'GolferProfile';
 
 describe('GolferList', () => {
   it('should exist', () => {
     expect(GolferList).toExist();
   });
-})
+
+  it('should render one GolferProfile for each player', () => {
+    var players = [{
+      name: 'Bubba Watson',
+      age: 33,
+      nationality: 'USA',
+      fedexPoints: -230,
+      fedexRank: 4,
+      careerEarnings: 132232,
+      id: 123
+    },
+    {
+      name: 'Jordan Speith',
+      age: 27,
+      nationality: 'USA',
+      fedexPoints: -230,
+      fedexRank: 4,
+      careerEarnings: 134123,
+      id: 234
+    }];
+
+    var golferList = TestUtils.renderIntoDocument(<GolferList players={players}/>);
+    var golferComponents = TestUtils.scryRenderedComponentsWithType(golferList, GolferProfile);
+
+    expect(golferComponents.length).toBe(players.length);
+
+  });
+
+}); // desribe GolferList
