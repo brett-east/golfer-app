a7ce02f98df95c3d46b1aad16701582ad4339d04
Brett East <brett@sproutback.com>
2017-04-21 21:44:03 +1000

    Add search/filter functionality

--- app/components/GolferApp.jsx
+++ app/components/GolferApp.jsx
@@ -7,6 +7,7 @@ class GolferApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      searchText: '',
       players: [{
         name: 'Bubba Watson',
         age: 33,
@@ -26,16 +27,36 @@ class GolferApp extends React.Component {
         id: 234
       }]
     };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  filterGolfers(players, searchText) {
+    var filteredGolfers = players;
+    filteredGolfers = filteredGolfers.filter((player) => {
+      if (searchText.length < 1) {
+        return true;
+      }
+      return player.name.toLowerCase().indexOf(searchText) >= 0;
+    });
+    return filteredGolfers;
+  }
+
+  handleSearch(searchText) {
+    this.setState({
+      searchText: searchText.toLowerCase()
+    });
   }
 
   render() {
+    var {players, searchText} = this.state;
+    var filteredGolfers = this.filterGolfers(players, searchText);
     return (
   		<div>
         <div className="row">
           <div className="column small-centered small-12 medium-8 large-8">
             <div className="container">
-              <GolferSearch />
-              <GolferList {...this.state}/>
+              <GolferSearch onSearch={this.handleSearch}/>
+              <GolferList players={filteredGolfers}/>
             </div>
           </div>
         </div>
--- app/components/GolferSearch.jsx
+++ app/components/GolferSearch.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 class GolferSearch extends React.Component {
   constructor(props) {
     super(props);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+  handleSearch() {
+    var searchText = this.refs.searchText.value;
+
+    this.props.onSearch(searchText);
   }
   render() {
     return (
     	<div>
         <form>
-          <input type="text" />
+          <input type="search" ref="searchText" placeholder="Filter by player name" onChange={this.handleSearch}/>
           <button className="button">Search</button>
         </form>
       </div>
